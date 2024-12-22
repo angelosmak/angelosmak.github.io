9efdaf56b48e768f94cdcfe3e068f2c0b3ea936c
Angelos Makris <sade-1995@hotmail.com>
2024-12-22 17:11:22 +0200

    fixes nav scrolly

--- scripts.js
+++ scripts.js
@@ -4,34 +4,40 @@ const popupImage = document.getElementById('popup-image');
 const closeBtn = document.querySelector('.close-btn');
 
 
-
-  gridImages.forEach(img => {
-    img.addEventListener('click', () => {
-      const src = img.getAttribute('src');
-      popupImage.setAttribute('src', src);
-      imagePopup.style.display = 'block';
-    });
+gridImages.forEach(img => {
+  img.addEventListener('click', () => {
+    const src = img.getAttribute('src');
+    popupImage.setAttribute('src', src);
+    imagePopup.style.display = 'block';
   });
-
+});
+if (imagePopup) {
   closeBtn.addEventListener('click', () => {
     imagePopup.style.display = 'none';
   });
+}
 
 
 const currentYearElement = document.getElementById('currentYear');
-  if (currentYearElement) {
-    currentYearElement.textContent = new Date().getFullYear();
+if (currentYearElement) {
+  currentYearElement.textContent = new Date().getFullYear();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navigation = document.querySelector('.navigation');
+
+
+  if (navigation) {
+    console.log("Navigation element found.");
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 0) {
+        navigation.classList.add('nav-scrolled');
+        console.log(navigation);
+      } else {
+        navigation.classList.remove('nav-scrolled');
+      }
+    });
+  } else {
+    console.error("Navigation element not found. Check your HTML structure.");
   }
-
-document.addEventListener("DOMContentLoaded", function() {
-
-  const navigationWrap = document.querySelector('.navigation');
-
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-      navigationWrap.classList.add('nav-scrolled');
-    } else {
-      navigationWrap.classList.remove('nav-scrolled');
-    }
-  });
 });
